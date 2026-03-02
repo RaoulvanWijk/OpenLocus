218dcd121d4104f0616932f8d09c81d44dd12cea
Gijs Bartman <gijs.bartman@gmail.com>
2026-03-02 16:38:00 +0100

    feat: configure tanstack router and add shadcn to ui package

---
+++ apps/desktop/src/routes/index.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@openlocus/ui/components/button'
+import { Input } from '@openlocus/ui/components/input'
+import { createFileRoute } from '@tanstack/react-router'
+import { invoke } from '@tauri-apps/api/core'
+import { useState } from 'react'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState('')
+  const [name, setName] = useState('')
+
+  async function greet() {
+    setGreetMsg(await invoke('greet', { name }))
+  }
+
+  return (
+    <main>
+      <form
+        className="flex gap-2"
+        onSubmit={(e) => {
+          e.preventDefault()
+          greet()
+        }}
+      >
+        <Input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <Button type="submit">Greet</Button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  )
+}
--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,11 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -28,4 +37,4 @@ export default defineConfig(async () => ({
       ignored: ['**/src-tauri/**'],
     },
   },
-}))
+})
