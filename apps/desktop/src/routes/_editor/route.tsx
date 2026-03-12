e9e7453d3df884906d4e3e5e57e7a05db964bdfa
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2026-03-12 14:57:44 +0100

    feat: refactor RouteComponent to use NotesSidebar and EditorContextProvider

--- apps/desktop/src/routes/_editor/route.tsx
+++ apps/desktop/src/routes/_editor/route.tsx
@@ -1,62 +1,18 @@
-import { cn } from '@openlocus/ui/lib/utils'
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
-import { Route as EditRoute } from './edit.$id'
+import EditorContextProvider from '@/features/editor/editor-context'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { NotesSidebar } from '../../features/editor/sidebar/NotesSidebar'
 
 export const Route = createFileRoute('/_editor')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const { id } = EditRoute.useParams()
-
   return (
     <div className="flex">
-      <aside className="bg-sidebar">
-        <nav>
-          <Link to="/">Back home</Link>
-        </nav>
-        <nav>
-          <ul>
-            <li>
-              <Link
-                to="/edit/$id"
-                params={{ id: '123' }}
-                className={cn('', id === '123' && 'font-bold')}
-              >
-                Edit note 123
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/edit/$id"
-                params={{ id: '456' }}
-                className={cn('', id === '456' && 'font-bold')}
-              >
-                Edit note 456
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/edit/$id"
-                params={{ id: '789' }}
-                className={cn('', id === '789' && 'font-bold')}
-              >
-                Edit note 789
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/edit/$id"
-                params={{ id: '000' }}
-                className={cn('', id === '000' && 'font-bold')}
-              >
-                Edit note 000
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </aside>
-      <Outlet />
+      <EditorContextProvider>
+        <NotesSidebar />
+        <Outlet />
+      </EditorContextProvider>
     </div>
   )
 }
