178efc32f05f3d89eaf776118a6fb43c4418e058
Gijs Bartman <gijs.bartman@gmail.com>
2026-03-13 08:42:43 +0100

    fix: update NoteItem styles for improved visual consistency

--- apps/desktop/src/features/editor/sidebar/NoteItem.tsx
+++ apps/desktop/src/features/editor/sidebar/NoteItem.tsx
@@ -23,9 +23,9 @@ export default function NoteItem({ note }: NoteItemProps) {
       onClick={() => navigate({ to: '/notes/$id', params: { id: note.id } })}
       data-state={currentId === note.id ? 'selected' : 'deselected'}
       className={cn(
-        'group/note bg-sidebar relative z-10 cursor-pointer rounded-lg px-4 py-3 transition-colors duration-200',
+        'group/note bg-sidebar relative z-10 cursor-pointer rounded-lg border border-transparent px-4 py-3 transition-colors duration-200',
         'hover:bg-gray-200!',
-        'data-[state=selected]:border data-[state=selected]:bg-white data-[state=selected]:text-black data-[state=selected]:shadow-sm',
+        'data-[state=selected]:border-border data-[state=selected]:bg-white data-[state=selected]:text-black data-[state=selected]:shadow-sm data-[state=selected]:hover:bg-gray-100',
         'before:absolute before:top-0 before:bottom-0 before:left-0 before:my-auto before:h-8/12 before:w-0.75 before:rounded-r-full before:bg-[#1F2937] before:opacity-0 before:content-[""] data-[state=selected]:before:opacity-100',
       )}
     >
@@ -44,7 +44,7 @@ export default function NoteItem({ note }: NoteItemProps) {
           onClick={handleDeleteNote}
           className={cn(
             'hidden cursor-pointer rounded p-1 group-hover/note:block hover:bg-gray-300',
-            'group-hover/note:block group-data-[state=selected]/note:block hover:block',
+            'group-hover/note:block group-data-[state=selected]/note:block',
           )}
         >
           <Trash className="size-4" />
